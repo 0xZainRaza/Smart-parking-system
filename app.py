1e1052eaea358f1f35c65978cb818245de0dc6de
Zain <154006182+0xZainRaza@users.noreply.github.com>
2024-02-05 21:51:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config['MAIL_SERVER'] = 'smtp.office365.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'smartpakingsystem@outlook.com'
-app.config['MAIL_PASSWORD'] = 'smartparkingsystem123'  
+app.config['MAIL_PASSWORD'] = '-'  
 
 mail = Mail(app)
 
