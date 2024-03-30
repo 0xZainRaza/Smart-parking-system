791cbd1e9fe6bc3cf125c0978d528959c2443326
Osama Irfan <128910142+osamairfan9@users.noreply.github.com>
2024-03-30 13:56:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from flask_mail import Mail, Message
 import random
 import string
+from multichain import MultiChain
 
 app = Flask(__name__)
 CORS(app)
@@ -12,14 +13,22 @@ app.config['MAIL_SERVER'] = 'smtp.office365.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'smartpakingsystem@outlook.com'
-app.config['MAIL_PASSWORD'] = 'smartparkingsystem123'  
+app.config['MAIL_PASSWORD'] = 'smartparkingsystem123'
 
 mail = Mail(app)
+
+# Connect to Multichain
+multichain = MultiChain("chain1rpc", user="multichainrpc", password="rpcpassword", port=12345)
 
 def generate_verification_code(length=6):
     characters = string.ascii_lowercase + string.digits
     verification_code = ''.join(random.choice(characters) for i in range(length))
     return verification_code
+
+def publish_to_multichain(data):
+    stream_name = "user_registration_stream"
+    item = {'email': data['email'], 'verification_code': data['verification_code']}
+    multichain.publish(stream_name, "registration", item)
 
 @app.route('/submit-form', methods=['POST'])
 def submit_form():
@@ -41,6 +50,9 @@ def submit_form():
     try:
         mail.send(msg)
         print("Email sent successfully!")
+        # Publish to Multichain stream
+        data['verification_code'] = verification_code
+        publish_to_multichain(data)
         return jsonify({'message': 'Email sent successfully'}), 200
     except Exception as e:
         print(f"Error sending email: {e}")
